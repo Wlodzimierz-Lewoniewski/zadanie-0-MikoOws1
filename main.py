a3d259dbdd90110a8f771509ffa0bb04d0277399
Mikolaj Owsianny <83515@student.ue.poznan.pl>
2024-10-13 23:25:21 +0200

    add fix2

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-docs = int(input("liczba dokumentow: "))
+docs = int(input())
 documents = []
-for i in range(docs):
-    documents.append(input(f"dokument {i + 1}: "))
+for _ in range(docs):
+    documents.append(input())
 
-qs = int(input("liczba zapytan: "))
+qs = int(input())
 queries = []
-for i in range(qs):
-    queries.append(input(f"zapytanie {i + 1}: "))
+for _ in range(qs):
+    queries.append(input())
 
 def process_document(document):
     cleaned_document = "".join([char.lower() if char.isalnum() else " " for char in document])
@@ -17,12 +17,10 @@ doc_words = [process_document(doc) for doc in documents]
 for query in queries:
     results = []
     query_lower = query.lower()
-    index = 0
-    for words in doc_words:
+    for index, words in enumerate(doc_words):
         count = words.count(query_lower)
         if count > 0:
             results.append((index, count))
-        index += 1
     
     results.sort(key=lambda x: (-x[1], x[0]))
     
