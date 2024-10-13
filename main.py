2d8eb4325a228f9248e07f665e038b2eac1f67d7
Mikolaj Owsianny <83515@student.ue.poznan.pl>
2024-10-13 23:15:32 +0200

    add zadanie0

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+docs = int(input("liczba dokumentow: "))
+documents = []
+for i in range(docs):
+    documents.append(input(f"dokument {i + 1}: "))
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+qs = int(input("liczba zapytan: "))
+queries = []
+for i in range(qs):
+    queries.append(input(f"zapytanie {i + 1}: "))
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+def process_document(document):
+    cleaned_document = "".join([char.lower() if char.isalnum() else " " for char in document])
+    return cleaned_document.split()
+
+doc_words = [process_document(doc) for doc in documents]
+
+for query in queries:
+    results = []
+    query_lower = query.lower()
+    index = 0
+    for words in doc_words:
+        count = words.count(query_lower)
+        if count > 0:
+            results.append((index, count))
+        index += 1
+    
+    results.sort(key=lambda x: (-x[1], x[0]))
+    
+    result_final = [str(result[0]) for result in results]
+    print("wynik dla '", query, "':", " ".join(result_final))
